0bc5ae6137f7fa6b46069e76c3f41104f8bab43a
ChengliangQian <1096231200@qq.com>
2017-01-22 20:13:56 +0800

    转向测试

---
+++ _001-steerig.ino
@@ -0,0 +1,37 @@
+#include <Servo.h>
+
+Servo Steering0;
+String inString = "";    // string to hold input
+
+int pos=90;         //舵机角度
+
+void setup ()
+{
+   Serial.begin(9600);
+  Steering0.attach(5);
+  }
+
+void loop ()
+{
+  while (Serial.available() > 0) 
+  {
+    int inChar = Serial.read();
+    if (isDigit(inChar)) 
+    {
+      inString += (char)inChar; 
+    }
+
+    if (inChar == '\n') 
+    {
+      Serial.print("Value:");
+      Serial.println(inString.toInt());
+      pos=inString.toInt();
+        Steering0.write(pos);
+        delay(30);
+      // clear the string for new input:
+      inString = ""; 
+    }
+  }
+  }
+
+
